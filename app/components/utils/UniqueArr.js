9337a8c1123908aa111948f092d8624eb0c83241
hnguy0221 <hnguy0221@gmail.com>
2017-11-02 17:30:45 -0600

    Fixed tab and space issues

--- app/components/utils/UniqueArr.js
+++ app/components/utils/UniqueArr.js
@@ -1,23 +1,23 @@
 var UniqueArr = function(arr)
 {
-	var temp = [];
-	arr.forEach(function(el1) 
-	{
-		var unique = true;
-		temp.forEach(function(el2) 
-		{
-			if (el1.name === el2.name)
-			{
-				unique = false;
-			}
-		});
-		if (unique)
-		{
-			temp.push(el1);
-		}
-	});
+    var temp = [];
+    arr.forEach(function(el1) 
+    {
+        var unique = true;
+        temp.forEach(function(el2) 
+        {
+            if (el1.name === el2.name)
+            {
+                unique = false;
+            }
+        });
+        if (unique)
+        {
+            temp.push(el1);
+        }
+    });
 
-	return temp;
+    return temp;
 };
 
 module.exports = UniqueArr;
